0c0aba6a81a327096e8f08fd62a397400a4d8b79
unknownexception <unknownliveid@hotmail.com>
2012-02-27 22:21:22 +0700

    many many work for features - minumum: dispatcher + handler

--- HttpContext.fs
+++ HttpContext.fs
@@ -13,7 +13,7 @@ type HttpRequest =
         Url:string;
         Query:string;
         Method:HttpMethod;
-        Headers:string List;
+        Headers:seq<string>;
     }
 
 type RequestData =
@@ -27,5 +27,26 @@ type HandlerFunction = (RequestData -> ResponseData)
 type DispatchEntry = (string * HandlerFunction)
 type DispatchEntryEx = (Regex * HandlerFunction)
 
+
+let split (sep:string) (str:string) =
+    match sep, str with
+    | ((null | ""), _) | (_, (null | "")) -> seq [str]
+    | _ ->
+        let n, j = str.Length, sep.Length
+        let rec loop p = 
+            seq {
+                if p < n then
+                    let i = match str.IndexOf(sep, p) with -1 -> n | i -> i
+                    yield str.Substring(p, i - p)
+                    yield! loop (i + j)
+            }
+        loop 0
+
+
 let ParseRequest rawData = 
+    let seqHeaders = split "\r\n" rawData
+    let mainHeader = Seq.nth 1 seqHeaders     
+    match mainHeader with
+        | (null | "") -> "no header"
+        | _ -> "something exist"
     
